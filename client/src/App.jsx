3adf582ae1f534f42794a04663795e04042cb352
aunshpatel <patelaunsh28@gmail.com>
2023-11-30 22:04:01 -0500

    Add routes and create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,11 +8,11 @@ import Profile from './pages/Profile';
 export default function App() {
   return (<BrowserRouter>
     <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/sign-up" element={<SignUp />} />
-    <Route path="/sign-in" element={<SignIn />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>);
 }
