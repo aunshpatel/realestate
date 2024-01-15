397cfb95ab617d4cf3b89b910c05befa35b9b496
aunshpatel <patelaunsh28@gmail.com>
2024-01-14 21:33:10 -0500

    Sign In UI created

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) =>{
+    setFormData({
+      ...formData, 
+      [e.target.id]: e.target.value,
+    });
+  }
+  console.log(formData)
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+    setLoading(true);
+    const res = await fetch('/api/auth/signin', {
+      method: 'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await res.json();
+    if(data.success === false){
+      setError(data.message);
+      setLoading(false);
+      return;
+    }
+    setLoading(false);
+    setError(null);
+    navigate('/sign-in');
+  }
+
   return (
-    <h1>
-      SignIn
-    </h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        <input type="email" name="email" id="email" placeholder='email' className='border p-3 rounded-lg'  onChange={handleChange}/>
+
+        <input type="password" name="password" id="password" placeholder='password' className='border p-3 rounded-lg' onChange={handleChange}/>
+
+        <button disabled={loading} id="signUp" className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:placeholder-opacity-95 disabled:placeholder-opacity-80' onChange={handleChange}>
+          {loading?'Loading':'Sign In'}
+        </button>
+        {error && <p className='text-red-500 mt-5'>{error}</p>}
+      </form>
+
+      <div className='flex gap-2 mt-5'>
+        <p>
+          New Here?
+        </p>
+        <Link to={"/sign-up"}>
+          <span className='text-blue-700'>Sign Up Here</span>
+        </Link>
+      </div>
+    </div>
   )
 }
