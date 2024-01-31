6758f6e41c9a0ef261baa24f6a1c4a49e86d79a4
aunshpatel <patelaunsh28@gmail.com>
2024-01-30 21:11:10 -0500

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,10 +1,15 @@
 import {useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user)
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) =>{
     setFormData({
@@ -15,26 +20,33 @@ export default function SignIn() {
 
   const handleSubmit = async (e) =>{
     e.preventDefault();
-    setLoading(true);
-    const res = await fetch('/api/auth/signin', {
-      method: 'POST',
-      headers:{
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(formData),
-    });
-
-    const data = await res.json();
-    if(data.success === false){
-      setLoading(false);
-      setError(data.message);
-      // setError('Wrong email id and/or password. Please check and enter the correct values.');
-      return;
+    try{
+      // setLoading(true);
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers:{
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData),
+      });
+  
+      const data = await res.json();
+      if(data.success === false){
+        // setLoading(false);
+        // setError(data.message);
+        dispatch(signInFailure(data.message));
+        // setError('Wrong email id and/or password. Please check and enter the correct values.');
+        return;
+      }
+      // setLoading(false);
+      // setError(null);
+      dispatch(signInSuccess(data));
+      alert('Congratulations, you have signed in successfully. You will now be redirected to home page.');
+      navigate('/');
+    } catch(error){
+      dispatch(signInFailure(error.message))
     }
-    setLoading(false);
-    setError(null);
-    alert('Congratulations, you have signed in successfully. You will now be redirected to home page.');
-    navigate('/');
   }
 
   return (
