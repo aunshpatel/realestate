9e77f49e736b16e2e2f17649650dfd1fcddf57d3
aunshpatel <patelaunsh28@gmail.com>
2024-02-05 06:32:18 -0500

    Create 'Update User' api and route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,15 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouters from './routes/userRoutes.js';
 import authRouter  from './routes/authRoute.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-const app=express();
+const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
+
 
 mongoose.connect(process.env.MongoDB).then(()=>{
     console.log('Connected to MongoDB');
