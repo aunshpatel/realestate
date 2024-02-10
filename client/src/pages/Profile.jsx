1431a6963e76a0ea13949ae75f9123c8a7ce5274
aunshpatel <patelaunsh28@gmail.com>
2024-02-10 17:23:00 -0500

    Complete 'Show user listing'

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,7 +14,9 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const [updateFail, setUpdateFail] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
   const dispatch = useDispatch(); 
 
   useEffect(()=>{
@@ -111,7 +113,7 @@ export default function Profile() {
     }
   }
 
-  const handleSignOut = async() =>{
+  const handleSignOut = async () => {
     try{
       dispatch(signoutUserStart());
       const res = await fetch('/api/auth/signout');
@@ -125,6 +127,22 @@ export default function Profile() {
 
     } catch(error){
       dispatch(signoutUserFailure(error.message));
+    }
+  }
+
+  const handleShowListings = async () => {
+    try{ 
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if(data.success === false){
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch(error){
+      setShowListingsError(true);
     }
   }
 
@@ -174,6 +192,40 @@ export default function Profile() {
           Sign Out
         </span>
       </div>
+      <button onClick={handleShowListings} className='text-green-700 w-full'>Show Listings</button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings!' : ''}
+      </p>
+      {
+        userListings && userListings.length > 0 && 
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {
+            userListings.map((listing) =>(
+              <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+                <Link to={`/listing/${listing._id}`}>
+                  <img src={listing.imageUrls[0]} alt='Listing Cover Image' className='h-20 w-20 object-contain rounded-lg'/>
+                </Link>
+                <Link className='flex-1 text-slate-700 font-semibold hover:underline truncate' to={`/listing/${listing._id}`}>
+                  <p className=''>
+                    {listing.name}
+                  </p>
+                </Link>
+                <div className='flex flex-col item-center'>
+                  <button className='text-red-700 uppercase'>
+                    Delete
+                  </button>
+                  <button className='text-green-700 uppercase'>
+                    Edit
+                  </button>
+                </div>
+              </div> 
+            ))
+          }
+      </div>
+      }
    </div>
   )
 }
