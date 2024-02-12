0252178ac49db8e123fbf63224d5f18effde9860
aunshpatel <patelaunsh28@gmail.com>
2024-02-12 18:30:26 -0500

    Update header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -66,6 +66,9 @@ export default function Header() {
                 <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
+                <Link to={"/all-listings"}>
+                  <li className='hidden sm:inline text-slate-700 hover:underline'>All Listings</li>
+                </Link>
                 <Link to='/sign-in'>
                   <li className='text-slate-700 hover:underline'>Sign In</li>
                 </Link>
