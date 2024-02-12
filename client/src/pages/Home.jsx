5c32e8a2ce7609d7b310e350f03ffc3d0f3656c0
aunshpatel <patelaunsh28@gmail.com>
2024-02-12 08:22:06 -0500

    Update Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 export default function Home() {
   return (
-    <h1>
-      Home
-    </h1>
+    <div>
+      {/* top */}
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+        <h1 className='text-slate-700 font-bold text-3xl lg:text-5xl'>
+          Find your next <span className='text-slate-500'>adobe</span> <br />
+          with ease.
+        </h1>
+        <div className='text-color-400 text-xs sm:text-lg'>
+          RealEstate will help you find your next home with comfort and ease. <br />
+          There is a large range of properties to choose from.
+        </div>
+
+        <Link to={'/search'} className='text-xs sm:text-lg text-blue-800 font-bold hover:underline'>
+          Let's get started!
+        </Link>
+      </div>
+
+      {/* swiper */}
+
+
+      {/* listing results for offer/sell/rent */}
+    </div>
   )
 }
