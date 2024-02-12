d2dfdf60f182c3d6cf30493b889213a371556c78
aunshpatel <patelaunsh28@gmail.com>
2024-02-12 18:19:40 -0500

    Remove Searchbar from header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,12 +33,12 @@ export default function Header() {
                 <span className='text-slate-700'>Estate</span>
             </h1>
         </Link>
-        <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        {/* <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
             <input type="text" placeholder='Search' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} className='bg-transparent focus:outline-none w-24 sm:w-64'/>
             <button>
               <FaSearch className='text-slate-600' />
             </button>
-        </form>
+        </form> */}
         {
           currentUser ? (
             <ul className='flex gap-4'>
