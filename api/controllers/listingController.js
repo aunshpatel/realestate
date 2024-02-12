a1b54f0d8d2dafc82716f7a2633edddb4221a10e
aunshpatel <patelaunsh28@gmail.com>
2024-02-11 23:50:52 -0500

    Create search api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -42,6 +42,7 @@ export const updateListing = async(req, res, next) => {
   }
 }
 
+//Single listing
 export const getListing = async(req, res, next) => {
   try {
     const listing = await Listing.findById(req.params.id);
@@ -52,5 +53,45 @@ export const getListing = async(req, res, next) => {
 
   } catch (error) {
     
+  }
+}
+
+
+//Multiple listings
+export const getListings = async(req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let discount = req.query.discount;
+    if(discount === undefined || discount === 'false'){
+      discount = { $in:[false, true] };
+    }
+
+    let furnished = req.query.furnished;
+    if(furnished === undefined || furnished === ''){
+      furnished = { $in:['SemiFurnished', 'Unfurnished', 'Furnished'] };
+    }
+
+    let type = req.query.type;
+    if(type === undefined || type === 'all') {
+      type = { $in:['sale', 'rent'] }
+    }
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      name: { $regex: searchTerm, $options: 'i' },
+      discount,
+      furnished,
+      type
+    }).sort({[ sort]: order }).limit(limit).skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
   }
 }
