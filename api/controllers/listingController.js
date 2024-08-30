0ca5bfebf11a1a107d4a091b9cbe8525362b22b5
aunshpatel <patelaunsh28@gmail.com>
2024-08-29 23:50:53 -0400

    Update verifyToken code for updateListing

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -31,9 +31,9 @@ export const updateListing = async(req, res, next) => {
   if(!listing){
     return next(errorHandler(404, 'Listing not found!'));
   }
-  if(req.user.id !== listing.userRef){
-    return next(errorHandler(401, 'You can only delete your own listing'));
-  }
+  // if(req.user.id !== listing.userRef){
+  //   return next(errorHandler(401, 'You can only delete your own listing'));
+  // }
   try {
     const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {new:true});
     res.status(200).json(updatedListing);
