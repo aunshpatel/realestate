b5ee19feb26e1c441a4f1365e9026f6eef9a5541
aunshpatel <patelaunsh28@gmail.com>
2024-09-06 02:00:24 -0400

    UI Update for listing details

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -10,28 +10,14 @@ export default function ListingItem({listing}) {
 
                 <div className='p-3 flex flex-col gap-2 w-full'>
                     <p className='text-lg font-semibold text-slate-700 truncate'>{listing.name}</p>
-                    <div className='flex items-center gap-1'>
-                        <MdLocationOn className='h-4 w-4 text-green-700'/>
-                        <p className='text-sm text-gray-600 truncate w-full'>{listing.address}</p>
-                    </div>
                     <p className='text-sm text-gray-600 line-clamp-2'>
                         {listing.description}
                     </p>
-                    <p className='text-slate-500 mt-2 font-semibold flex flex-row gap-4 justify-between'>
-                        <span>
-                        {
-                            listing.discount ? 
-                                <span> 
-                                    <span className='line-through'>{listing.selectedCurrency} {listing.regularPrice.toLocaleString('en-US')}</span> {listing.selectedCurrency} {listing.discountPrice.toLocaleString('en-US')}
-                                </span> : 
-                                <span>
-                                    {listing.selectedCurrency} {listing.regularPrice.toLocaleString('en-US')}
-                                </span>
-                        }
-                        {listing.type === 'rent' && ' / month'}
-                        </span>
-                    </p>
-                    <div className='text-slate-700 flex gap-4'>
+                    <div className='flex mt-2 items-center gap-1'>
+                        <MdLocationOn className='h-4 w-4 text-green-700'/>
+                        <p className='text-sm text-gray-600 truncate w-full'>{listing.address}</p>
+                    </div>
+                    <div className='text-slate-700 mt-2 flex gap-4'>
                         <div className='font-semibold text-sm'>
                             {
                                 listing.bedrooms > 1 ? `${listing.bedrooms} bedrooms` :  `${listing.bedrooms} bedroom`
@@ -43,12 +29,30 @@ export default function ListingItem({listing}) {
                             }
                         </div>
                     </div>
+                    <p className='text-sm mt-2 text-gray-600 line-clamp-2'>
+                        {listing.furnished}
+                    </p>
+                    <p className='text-slate-500 mt-2 font-semibold flex flex-row gap-4 justify-between'>
+                        <span>
+                        {
+                            listing.discount ? 
+                                <span> 
+                                    <span className='line-through'>{listing.selectedCurrency} {listing.regularPrice.toLocaleString('en-US')}</span> <br /> {listing.selectedCurrency} {listing.discountPrice.toLocaleString('en-US')}
+                                </span> : 
+                                <span>
+                                    {listing.selectedCurrency} {listing.regularPrice.toLocaleString('en-US')}
+                                </span>
+                        }
+                        {listing.type === 'rent' && ' / month'}
+                        </span>
+                    </p>
+                    
                     <div>
                     {listing.type === 'rent' ?  
-                        (<p className='bg-slate-500 max-w-[150px] text-white text-center text-md p-2 rounded-md'>
+                        (<p className='bg-slate-500 mt-2 max-w-[150px] text-white text-center text-md p-2 rounded-md'>
                              For Rent
                         </p>) : 
-                        (<p className='bg-green-900 max-w-[150px] text-white text-center text-md p-2 rounded-md'>
+                        (<p className='bg-green-900 mt-2 max-w-[150px] text-white text-center text-md p-2 rounded-md'>
                             For Sale
                         </p>)
                     }
