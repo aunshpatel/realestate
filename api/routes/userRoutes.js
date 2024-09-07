b41b694663bf32348f9d5ab54d646334b84a5496
aunshpatel <patelaunsh28@gmail.com>
2024-09-07 02:43:35 -0400

    Remove verifyToken

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -6,7 +6,10 @@ const router = express.Router();
 
 // router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser);
-router.delete('/delete/:id', verifyToken, deleteUser);
+
+// router.delete('/delete/:id', verifyToken, deleteUser);
+router.delete('/delete/:id', deleteUser);
+
 //The following will get all the listings of a logged in user
 // router.get("/listings/:id", verifyToken, getUserListings);
 router.get("/listings/:id", getUserListings);
