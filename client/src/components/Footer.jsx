6b287b3fca8090dbaf0545a339c177e2ee4f3a2d
aunshpatel <patelaunsh28@gmail.com>
2024-09-13 02:59:25 -0400

    UI Update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,8 +7,8 @@ import appStore from "../assets/images/coming-soon-to-apple.jpeg";
 export default function Footer() {
     return(
         <>
-            <div className='flex xs:flex-col sm:flex-row justify-between bg-slate-200 sm:h-[76px] w-[100%] fixed bottom-0 z-10'>
-                <div className="flex xs:flex-col sm:flex-row items-center xs:p-6 sm:p-4">
+            <div className='flex xs:flex-col sm:flex-row justify-between bg-slate-200 xs:h-[30%] sm:h-[76px] w-[100%] lg:fixed bottom-0 z-1'>
+                <div className="flex xs:flex-col sm:flex-row items-center xs:p-8 sm:p-4">
                     <ul className='flex xs:flex-col sm:flex-row xs:space-y-8 sm:space-x-8'>
                         <Link to='/mobileapp-privacy-policy' className="border-b border-gray-400 uppercase">
                             <li className='text-md'>Mobile App Privacy Policy</li>
@@ -23,15 +23,15 @@ export default function Footer() {
                         </Link>
                     </ul>
                 </div>
-                <div className="flex xs:flex-col sm:flex-row items-center xs:p-6 sm:p-4">
+                <div className="flex xs:flex-col sm:flex-row items-center p-4">
                     <ul className='flex xs:flex-col sm:flex-row xs:space-y-8 sm:space-x-8'>
-                        <Link to='/mobileapp-privacy-policy' className="border-b border-gray-400 uppercase">
+                        <Link to='/mobileapp-privacy-policy'>
                             <li className='text-md'>
                                 <img src={googlePlay} alt="Coming Soon to Google Play" className='h-[50px]'/>
                             </li>
                         </Link>
 
-                        <Link to='/delete-profile-instructions' className="border-b border-gray-400 uppercase">
+                        <Link to='/delete-profile-instructions'>
                             <img src={appStore} alt="Coming Soon to App Store" className='h-[50px]'/>
                         </Link>
                     </ul>
--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -8,26 +8,26 @@ export default function ContactForm() {
   }
   return (
     <>
-        <div className='justify-center items-center flex flex-col mb-6 mt-6'>
-            <div className="bg-white justify-center items-center w-max shadow-md rounded-lg p-8">
+        <div className='justify-center items-center flex flex-col m-6 h-fit'>
+            <div className="bg-white justify-center items-center xs:w-[100%] sm:w-max shadow-md rounded-lg p-8">
                 <h1 className="text-xl font-bold mb-4">Contact Us</h1>
                 <form className='flex flex-col' action='https://formspree.io/f/xblrejoj' method='post' encType='multipart/form-data'>
                     <div className='mb-5 border-collapse border-2'>
-                        <input id='name' type='text' name='name' placeholder='Your Name' className=' w-96' />
+                        <input id='name' type='text' name='name' placeholder='Your Name' className='xs:w-[100%] w-96' />
                     </div>
                     <div className='mb-5 border-collapse border-2'>
-                        <input id='email' type='email' name='email' placeholder='Your Email ID' className=' w-96' />
+                        <input id='email' type='email' name='email' placeholder='Your Email ID' className='xs:w-[100%] w-96' />
                         <ValidationError prefix="Email" field="email"  errors={state.errors} />
                     </div>
                     <div className='mb-5 border-collapse border-2'>
-                        <textarea id='message' rows='5' className='w-96' name='message' placeholder='Your Message' />
+                        <textarea id='message' rows='5' className='xs:w-[100%] w-96' name='message' placeholder='Your Message' />
                         <ValidationError prefix="Message" field="message" errors={state.errors} />
                     </div>
-                    <button type="submit" className='mb-5 border-collapse border-2 w-96'>
+                    <button type="submit" className='mb-5 border-collapse border-2 xs:w-[100%] w-96'>
                         Submit
                     </button>
                 </form>
-                <p className='text-red-500 font-bold w-[400px]'>
+                <p className='text-red-500 font-bold xs:w-[100%] w-[400px]'>
                     Please note that once you press submit button, you will be redirected to a new page. To come back, you will have to press 'Go back' button. Alternatively, you can also close the tab.
                 </p>
             </div>
