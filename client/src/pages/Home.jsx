bbb4797cc632d3690be7cc50bfd78e9dc0fdb190
aunshpatel <patelaunsh28@gmail.com>
2025-01-25 17:46:53 +0530

    Spelling update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -55,7 +55,7 @@ export default function Home() {
       {/* top */}
       <div className='flex flex-col gap-6 p-40 px-3 max-w-6xl mx-auto'>
         <h1 className='text-slate-900 font-bold text-3xl lg:text-5xl'>
-          Find your next <span className='text-slate-400'>humble adobe</span> <br />
+          Find your next <span className='text-slate-400'>humble abode</span> <br />
           with ease.
         </h1>
         <div className='text-slate-800 text-md font-bold sm:text-lg'>
