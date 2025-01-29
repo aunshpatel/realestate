5c4c352b8db704ca1fe3b7b4957ea173aa7e8577
aunshpatel <patelaunsh28@gmail.com>
2025-01-29 14:59:13 +0530

    Add padding bottom

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -244,7 +244,7 @@ export default function UpdateListing() {
     }
 
     return (
-        <main className='p-3 max-w-4xl mx-auto'>
+        <main className='p-3 max-w-4xl mx-auto pb-40'>
             <h1 className='text-3xl font-semibold text-center my-7'>Update Listing</h1>
 
             <form onSubmit={handleSubmit} className='flex flex-col'>
